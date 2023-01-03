f871a3aa5fa65b411c21db7c77e4a6dadd6442ff
Karol Bisztyga <karol.bisztyga@swmansion.com>
2023-01-03 10:10:45 +0100

    save to a file in cli

--- crates/cairo-sierra-to-casm/src/cli.rs
+++ crates/cairo-sierra-to-casm/src/cli.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 use clap::Parser;
 use cairo_utils::logging::init_logging;
 use cairo_sierra_to_casm::compiler::{Args, compile_at_path};
@@ -8,5 +10,6 @@ fn main() {
 
     let args = Args::parse();
 
-    compile_at_path(&args.file).expect("compilation failed");
+    let cairo_program = compile_at_path(&args.file).expect("compilation failed");
+    fs::write(args.output, format!("{}", cairo_program)).expect("Failed to write output.");
 }
