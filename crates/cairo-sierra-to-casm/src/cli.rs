9746bc71a79ce80266c0f435ab9031669b9b4351
Maksymilian Demitraszek <32527780+MaksymilianDemitraszek@users.noreply.github.com>
2023-01-03 11:01:15 +0100

    Revert "Fix cargo"

--- crates/cairo-sierra-to-casm/src/cli.rs
+++ crates/cairo-sierra-to-casm/src/cli.rs
@@ -1,8 +1,6 @@
-use std::fs;
-
 use clap::Parser;
-use cairo_utils::logging::init_logging;
-use cairo_sierra_to_casm::compiler::{Args, compile_at_path};
+use utils::logging::init_logging;
+use sierra_to_casm::compiler::{Args, compile_at_path};
 
 fn main() {
     init_logging(log::LevelFilter::Off);
@@ -10,6 +8,5 @@ fn main() {
 
     let args = Args::parse();
 
-    let cairo_program = compile_at_path(&args.file).expect("compilation failed");
-    fs::write(args.output, format!("{}", cairo_program)).expect("Failed to write output.");
+    compile_at_path(args).expect("compilation failed");
 }
