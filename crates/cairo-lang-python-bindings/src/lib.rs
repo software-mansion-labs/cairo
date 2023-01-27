11339d4bd6811e7fb2494ef0e925dc9350c2f2b5
Karol Bisztyga <karol.bisztyga@swmansion.com>
2023-01-27 16:02:57 +0100

    fix rebase

--- crates/cairo-lang-python-bindings/src/lib.rs
+++ crates/cairo-lang-python-bindings/src/lib.rs
@@ -37,10 +37,6 @@ use cairo_lang_diagnostics::ToOption;
 mod find_tests;
 
 use find_tests::find_all_tests;
-use cairo_lang_syntax::node::ast::Expr;
-use cairo_lang_filesystem::ids::CrateId;
-use cairo_lang_semantic::db::SemanticGroup;
-use cairo_lang_syntax::node::Token;
 use cairo_lang_protostar::build_protostar_casm_from_file;
 use cairo_lang_semantic::{ConcreteFunction, FunctionLongId};
 use cairo_lang_semantic::items::functions::GenericFunctionId;
@@ -166,91 +162,6 @@ fn call_test_collector(path: &str, output_path: Option<&str>) -> PyResult<(Optio
         result_contents = Some(sierra_program.to_string());
     }
     Ok((result_contents, named_tests))
-}
-
-
-/// Expectation for a result of a test.
-enum TestExpectation {
-    /// Running the test should not panic.
-    Success,
-    /// Running the test should result in a panic.
-    Panics,
-}
-
-/// The configuration for running a single test.
-#[allow(dead_code)]
-struct TestConfig {
-    /// The function id of the test function.
-    func_id: FreeFunctionId,
-    /// The amount of gas the test requested.
-    available_gas: Option<usize>,
-    /// The expected result of the run.
-    expectation: TestExpectation,
-    /// Should the test be ignored.
-    ignored: bool,
-}
-
-/// Finds the tests in the requested crates.
-fn find_all_tests(db: &dyn SemanticGroup, main_crates: Vec<CrateId>) -> Vec<TestConfig> {
-    let mut tests = vec![];
-    for crate_id in main_crates {
-        let modules = db.crate_modules(crate_id);
-        for module_id in modules.iter() {
-            let Ok(module_items) = db.module_items(*module_id) else {
-                continue;
-            };
-
-            for item in module_items.iter() {
-                if let ModuleItemId::FreeFunction(func_id) = item {
-                    if let Ok(attrs) =
-                        db.function_with_body_attributes(FunctionWithBodyId::Free(*func_id))
-                    {
-                        let mut is_test = false;
-                        let mut available_gas = None;
-                        let mut ignored = false;
-                        let mut should_panic = false;
-                        for attr in attrs {
-                            match attr.id.as_str() {
-                                "test" => {
-                                    is_test = true;
-                                }
-                                "available_gas" => {
-                                    // TODO(orizi): Provide diagnostics when this does not match.
-                                    if let [Expr::Literal(literal)] = &attr.args[..] {
-                                        available_gas = literal
-                                            .token(db.upcast())
-                                            .text(db.upcast())
-                                            .parse::<usize>()
-                                            .ok();
-                                    }
-                                }
-                                "should_panic" => {
-                                    should_panic = true;
-                                }
-                                "ignore" => {
-                                    ignored = true;
-                                }
-                                _ => {}
-                            }
-                        }
-                        if is_test {
-                            tests.push(TestConfig {
-                                func_id: *func_id,
-                                available_gas,
-                                expectation: if should_panic {
-                                    TestExpectation::Panics
-                                } else {
-                                    TestExpectation::Success
-                                },
-                                ignored,
-                            })
-                        }
-                    }
-                }
-            }
-        }
-    }
-    tests
 }
 
 #[pyfunction]
