7162a794510ded691ecbef5e98d3cd95a2d70cb8
Karol Bisztyga <karol.bisztyga@swmansion.com>
2023-03-10 11:11:31 +0100

    follow the clippy err instruction

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+rustup component add clippy --toolchain nightly-x86_64-unknown-linux-gnu
+
 cargo clippy "$@" --all-targets --all-features -- -D warnings -D future-incompatible \
     -D nonstandard-style -D rust-2018-idioms -D unused
