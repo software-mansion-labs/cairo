3dcd9f21bbc6ff481b3b433aeb08d91ab5138692
Karol Bisztyga <karol.bisztyga@swmansion.com>
2023-05-10 11:55:16 +0200

    init

--- crates/cairo-lang-protostar/src/cli.rs
+++ crates/cairo-lang-protostar/src/cli.rs
@@ -19,9 +19,9 @@ struct Args {
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     let builtins = vec![String::from("GasBuiltin"), String::from("Pedersen"), String::from("RangeCheck"), String::from("bitwise"), String::from("ec_op")];
-    let (sierra_code_opt, collected) = collect_tests(&args.file, None, None, Some(builtins.iter().collect()))?;
+    let (sierra_program, collected) = collect_tests(&args.file, None, None, Some(builtins.iter().collect()))?;
 
-    let sierra_code = sierra_code_opt.ok_or(anyhow!("Expected sierra code"))?;
+    let sierra_code = sierra_program.to_string();
 
     if let Some(out_path) = args.output_sierra {
         fs::write(out_path, format!("{}", sierra_code)).context("Failed to write output.")?;
